8912098b1ef80350b72b4bdf2c27dc591fd7b1f1
Jeff <atomicxistence@gmail.com>
2019-02-17 15:57:06 -0500

    Initial Commit

---
+++ MathmaticalEquations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathmaticalEquations
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
